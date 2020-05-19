bd8ed2b5d159e5b5d96cff2fe12dbf1d7e6f1940
alansarluv <alan.sarluf@gmail.com>
2020-05-19 10:31:02 +0700

    set style background

--- content.js
+++ content.js
@@ -27,145 +27,203 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   }
 });
 
-const changeColorDom = (selected_color) => {
-  // convert theme text to color
-  const listTheme = {
-    "mekari": "#8763A9",
-    "talenta": "#C02A34",
-    "sleekr": "#018E57",
-    "jurnal": "#009bde",
-    "klikpajak": "#F57A1E"
-  }
-  const colorId = listTheme[selected_color];
 
-  // for intercom
-  let loopz = 0;
-  const intercomInterval = setInterval(() => {
-    loopz++;
-    if ( $('.intercom-lightweight-app-launcher.intercom-launcher').length ) {
-      $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
-      clearInterval(intercomInterval);
-    } else if (loopz > 25) {
-      clearInterval(intercomInterval);
+/* ===============================================
+  function for changeColorDom in all pages
+=============================================== */ 
+  // create an object for collect style custom, append all custom style right there
+  // in the end of line, concat that object and prepend that style in body page
+
+  const changeColorDom = (selected_color) => {
+    // convert theme text to color
+    const listTheme = {
+      "mekari": "#8763A9",
+      "talenta": "#C02A34",
+      "sleekr": "#018E57",
+      "jurnal": "#009bde",
+      "klikpajak": "#F57A1E"
     }
-    $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
-  }, 1000);
-  
-  // for alert div if any 
-  const alertDivInterval = setInterval(() => {
-    loopz++;
-    if ($('#warningPop').length) {
-      $('#warningPop').css("background-color", colorId); 
-      clearInterval(alertDivInterval);
-    } else if ($('#warningpop').length) {
-      $('#warningpop').css("background-color", colorId); 
-      clearInterval(alertDivInterval);
-    } else if (loopz > 15) {
-      clearInterval(alertDivInterval);
-    }
-    $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
-  }, 1000);
-  
-  // for background pattern
-  let stylePattern = `
-    <div
-      class="pattern-style"
-      style="
-        display: block;
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 184px;
-        background-color: ${colorId};
-        z-index: -1;
-      ">
-    </div>`;
-  
-  if (selected_color === 'talenta') {
-    stylePattern = `
-      <div
-        class="pattern-style"
-        style="
-          display: block;
-          position: absolute;
-          left: 0;
-          top: 0;
-          width: 100%;
-          height: 184px;
-          background: url(https://talenta.s3-ap-southeast-1.amazonaws.com/assets/images/img_talenta_pattern_red.png);
-          background-repeat: repeat;
-          background-size: 50px;
-          background-color: ${colorId};
-          z-index: -1;
-        ">
-      </div>`;
-  }
+    const colorId = listTheme[selected_color];
 
-  // append stylePattern inside main
-  $('main .pattern-style').remove();
-  $('main').prepend(stylePattern);
-};
-
-const triggerVoice = document.querySelector('.tl-dashboard-header small.text-dark');
-
-const startRecognition = () => {
-  recognition.start();
-}
-
-// let SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-let recognition = new webkitSpeechRecognition();
-recognition.continuous = false;
-recognition.lang = 'id-ID';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
-
-recognition.onresult = function(event) {
-  const rec = event.results[0][0].transcript;
-  const transcript = rec.toLocaleLowerCase();
-  // const confidence = event.results[0][0].confidence;
-  if (transcript.length) {
-    commandRun(transcript);
-  }
-}
-
-const voiceToggle = (val) => {
-  if (val) {
-    triggerVoice.addEventListener('click', startRecognition);
-  } else {
-    triggerVoice.addEventListener('click', startRecognition);
-  }
-}
-
-const commandRun = (val) => {
-  console.log("you said : " + val); 
-
-  const splitKey = val.split("luna ");
-  if (splitKey.length > 1) {
-    const commandKey = splitKey[1];
-    if (commandKey.includes("ganti tema")) {
-      const splitCommand = commandKey.split("ganti tema ");
-      const themeValText = splitCommand[1];
-      if (
-          themeValText == "sneaker" ||
-          themeValText == "speaker" ||
-          themeValText == "flickr"  ||
-          themeValText == "trigger" ||
-          themeValText == "stiker"  ||
-          themeValText == "sliker"
-        ) {
-        changeColorDom("sleekr");
-        chrome.storage.sync.set({'theme_val': "sleekr"});
-      } else {
-        changeColorDom(themeValText);
-        chrome.storage.sync.set({'theme_val': themeValText});
+    // for intercom if any
+    let loopz = 0;
+    const intercomInterval = setInterval(() => {
+      loopz++;
+      if ( $('.intercom-lightweight-app-launcher.intercom-launcher').length ) {
+        $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
+        clearInterval(intercomInterval);
+      } else if (loopz > 25) {
+        clearInterval(intercomInterval);
       }
-    } else if (commandKey.includes("saldo cuti")) {
-      $('.tl-dashboard-right p[data-target="#leaveDetailModal"] i').click()
-    } else if (commandKey.includes("cuti minggu ini")) {
-      $('.tl-dashboard-right .justify-content-between .btn-group .dropdown-menu a')[1].click();
-    } else if (commandKey.includes("cuti hari ini")) {
-      $('.tl-dashboard-right .justify-content-between .btn-group .dropdown-menu a')[0].click();
+      $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
+    }, 1000);
+    
+    // for alert div if any 
+    const alertDivInterval = setInterval(() => {
+      loopz++;
+      if ($('#warningPop').length) {
+        $('#warningPop').css("background-color", colorId); 
+        clearInterval(alertDivInterval);
+      } else if ($('#warningpop').length) {
+        $('#warningpop').css("background-color", colorId); 
+        clearInterval(alertDivInterval);
+      } else if (loopz > 15) {
+        clearInterval(alertDivInterval);
+      }
+      $('.intercom-lightweight-app-launcher.intercom-launcher').css("background-color", colorId); 
+    }, 1000);
+    
+    // for background pattern in dashboard if any
+    if ($('main').length) {
+      let stylePattern = `
+        <div
+          class="pattern-style"
+          style="
+            display: block;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 184px;
+            background-color: ${colorId};
+            z-index: -1;
+          ">
+        </div>`;
+      
+      if (selected_color === 'talenta') {
+        stylePattern = `
+          <div
+            class="pattern-style"
+            style="
+              display: block;
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 184px;
+              background: url(https://talenta.s3-ap-southeast-1.amazonaws.com/assets/images/img_talenta_pattern_red.png);
+              background-repeat: repeat;
+              background-size: 50px;
+              background-color: ${colorId};
+              z-index: -1;
+            ">
+          </div>`;
+      }
+  
+      // append stylePattern inside main
+      $('main .pattern-style').remove();
+      $('main').prepend(stylePattern);
+    }
+
+    // for theme in company index if any (.card-panel.payrollItem)
+    const cardPanelPayrollItem = $('.card-panel.payrollItem');
+    if (cardPanelPayrollItem.length) {
+      const styleCustom = `
+        <style>
+          .card-panel.payrollItem {
+            border-radius: 4px;
+          }
+          .card-panel.payrollItem:hover {
+            background: ${colorId}
+          }
+        </style>
+      `;
+      $('body').prepend(styleCustom);
+      console.log("ada");
+      // append bunch of style
+    }
+  };
+/* ===============================================
+   ------- end of changeColorDom function --------
+=============================================== */ 
+
+
+/* ===============================================
+  function for voiceToggle in all pages
+=============================================== */ 
+  const voiceToggle = (val) => {
+    const triggerVoice = document.querySelector('.tl-dashboard-header small.text-dark');
+
+    // only start if pages has that trigger
+    if (triggerVoice) {
+      if (val) {
+        triggerVoice.addEventListener('click', startRecognition);
+      } else {
+        triggerVoice.removeEventListener('click', startRecognition);
+      }
     }
   }
-}
+
+  // let SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  let recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.lang = 'id-ID';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  // activate voice recognition
+  const startRecognition = () => {
+    recognition.start();
+  }
+
+  // onresult will fired once a successful result is received
+  recognition.onresult = function(event) {
+    const rec = event.results[0][0].transcript;
+    const transcript = rec.toLocaleLowerCase();
+    // const confidence = event.results[0][0].confidence;
+    if (transcript.length) {
+      commandRun(transcript);
+    }
+  }
+
+  // once a single word has been recognised and it has finished being spoken,
+  // this handler is fired to stop the speech recognition service from running.
+  recognition.onspeechend = function() {
+    recognition.stop();
+  }
+
+  // supposed to handle cases where speech was recognised an error occured
+  // after 8s in silence, this function would be trigered
+  recognition.onerror = function(event) {
+    let errorText = event.error;
+    if (event.error === "no-speech") errorText = "Tidak ada suara"
+    console.log(errorText);
+  }
+
+  // convert fetch text to run command
+  const commandRun = (val) => {
+    console.log("you said : " + val); 
+
+    const splitKey = val.split("luna ");
+    if (splitKey.length > 1) {
+      const commandKey = splitKey[1];
+      if (commandKey.includes("ganti tema")) {
+        const splitCommand = commandKey.split("ganti tema ");
+        const themeValText = splitCommand[1];
+        if (
+            themeValText == "sneaker" ||
+            themeValText == "speaker" ||
+            themeValText == "flickr"  ||
+            themeValText == "trigger" ||
+            themeValText == "stiker"  ||
+            themeValText == "sliker"
+          ) {
+          changeColorDom("sleekr");
+          chrome.storage.sync.set({'theme_val': "sleekr"});
+        } else {
+          changeColorDom(themeValText);
+          chrome.storage.sync.set({'theme_val': themeValText});
+        }
+      } else if (commandKey.includes("saldo cuti")) {
+        $('.tl-dashboard-right p[data-target="#leaveDetailModal"] i').click()
+      } else if (commandKey.includes("cuti minggu ini")) {
+        $('.tl-dashboard-right .justify-content-between .btn-group .dropdown-menu a')[1].click();
+      } else if (commandKey.includes("cuti hari ini")) {
+        $('.tl-dashboard-right .justify-content-between .btn-group .dropdown-menu a')[0].click();
+      }
+    }
+  }
+
+/* ===============================================
+   --------- end of voiceToggle function ---------
+=============================================== */ 
